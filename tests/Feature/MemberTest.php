4b247b2e109b4f2c54a2ee9aa7648bf0a63378fc
boniich <ezequieldbo25@gmail.com>
2023-08-06 19:12:40 -0300

    fix: fix problems about images

--- tests/Feature/MemberTest.php
+++ tests/Feature/MemberTest.php
@@ -16,27 +16,31 @@ class MemberTest extends TestCase
 
     public function test_get_all_member_successfully(): void
     {
-        Member::factory(1)->create(['id' => 1]);
+        $memberData = Member::factory(1)->create(['id' => 1]);
+
         $response = $this->getJson($this->url);
 
         $response->assertJson(['success' => true, 'data' => [['id' => 1]], 'message' => 'Members retrived successfully'])->assertStatus(200);
+        destroyImagesInTests($memberData);
     }
 
     public function test_get_one_member_by_id_successfully(): void
     {
-        Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando dias']);
+        $memberData = Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando dias']);
         $response = $this->getJson($this->url . '1');
 
         $response->assertJson(['success' => true, 'data' => ['id' => 1, 'full_name' => 'Fernando dias'], 'message' => 'Member retrived successfully'])->assertStatus(200);
+        destroyImagesInTests($memberData);
     }
 
     public function test_not_found_member(): void
     {
-        Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando dias']);
+        $memberData = Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando dias']);
         $response = $this->getJson($this->url . '5');
 
 
         $response->assertJson(['success' => false, 'error' => 'Member not found'])->assertStatus(404);
+        destroyImagesInTests($memberData);
     }
 
     public function test_create_member_successfully(): void
@@ -53,13 +57,14 @@ class MemberTest extends TestCase
 
     public function test_update_member_successfully(): void
     {
-        Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando Dias']);
+        $memberData = Member::factory(1)->create(['id' => 1, 'full_name' => 'Fernando Dias']);
         $response = $this->putJson($this->url . '1', [
             'full_name' => 'Maria Dias',
         ]);
 
         $response->assertJson(['success' => true, 'data' => ['full_name' => 'Maria Dias'], 'message' => 'Member updated successfully'])
             ->assertStatus(200);
+        destroyImagesInTests($memberData);
     }
 
     public function test_not_found_member_to_update(): void
